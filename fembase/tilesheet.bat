7c91901f2479d55e27d4772f774b07762f97dbbb
clintbellanger <clintbellanger@gmail.com>
2017-05-27 20:25:06 -0500

    Block, Hit, Backswing animations

--- fembase/tilesheet.bat
+++ fembase/tilesheet.bat
@@ -3,6 +3,18 @@ del Run*17.png
 del Idle*17.png
 del Kneel*17.png
 del Dash*17.png
+del Block*9.png
+del Block*11.png
+del Block*13.png
+del Block*15.png
+del Block*17.png
+del Hit*9.png
+del Hit*11.png
+del Hit*13.png
+del Hit*15.png
+del Hit*17.png
+del Backswing*17.png
+
 
 montage -background "transparent" -tile 8x8 -geometry 256x256 Idle*.png _1idle.png
 
@@ -14,4 +26,10 @@ montage -background "transparent" -tile 8x8 -geometry 256x256 Kneel*.png _4die.p
 
 montage -background "transparent" -tile 8x8 -geometry 256x256 Dash*.png _5dash.png
 
-montage -background "transparent" -tile 5x1 _*.png -geometry 2048 -mode concatenate tilesheet.png
+montage -background "transparent" -tile 8x8 -geometry 256x256 Backswing*.png _6backswing.png
+
+montage -background "transparent" -tile 4x8 -geometry 256x256 Block*.png _7block.png
+
+montage -background "transparent" -tile 4x8 -geometry 256x256 Hit*.png _8hit.png
+
+montage -background "transparent" -tile 8x1 _*.png -geometry x2048 -mode concatenate tilesheet.png
